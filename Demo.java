d0c7c1fad7763234d7bedbb689f3751571bb94e4
Ellis Eghan <elliseghan@gmail.com>
2023-02-03 10:09:15 +0000

    implemented anagram checker using sorting approach

--- Demo.java
+++ Demo.java
@@ -1,4 +1,6 @@
-public class Demo{
+import java.util.Arrays;
+
+public class Demo {
     public static void main(String[] args) {
         Demo d = new Demo();
         String s1 = "garden";
@@ -6,8 +8,22 @@ public class Demo{
         d.isAnagram(s1, s2);
     }
 
-    public void isAnagram(String s1, String s2){
-        boolean result = false;
-        System.out.println(s1 + " and " + s2 +" are Anagrams?: " + result);
+    public void isAnagram(String s1, String s2) {
+        boolean result = true;
+        if (s1.length() != s2.length()) {
+            result = false;
+        } else {
+            char[] s1_char = s1.toCharArray();
+            char[] s2_char = s2.toCharArray();
+            Arrays.sort(s1_char);
+            Arrays.sort(s2_char);
+            for (int i = 0; i < s1_char.length; i++) {
+                if (s1_char[i] != s2_char[i]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(s1 + " and " + s2 + " are Anagrams?: " + result);
     }
 }
